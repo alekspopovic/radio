eb488efd88415916da7c9bf07c20ee62a4c00013
Aleks Popovic <aleksandar88popovic@gmail.com>
2021-03-05 22:45:42 +0100

    Radio app

---
+++ radio/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Radio from "./Radio";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Super-Duper Radio Player</h1>
+      <h2>Pick a genre, choose a station, start listening</h2>
+      <Radio />
+    </div>
+  );
+}
+
+export default App;
---
+++ radio/src/Radio.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { RadioBrowserApi } from "radio-browser-api";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import defaultImage from "./radio.jpg";
+
+export default function Radio() {
+  const [stations, setStations] = useState();
+  const [stationFilter, setStationFilter] = useState("all");
+
+  useEffect(() => {
+    setupApi(stationFilter).then((data) => {
+      console.log(data);
+      setStations(data);
+    });
+  }, [stationFilter]);
+
+  const setupApi = async (stationFilter) => {
+    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
+
+    const stations = await api
+      .searchStations({
+        language: "english",
+        tag: stationFilter,
+        limit: 30,
+      })
+      .then((data) => {
+        return data;
+      });
+
+    return stations;
+  };
+
+  const filters = [
+    "all",
+    "classical",
+    "country",
+    "dance",
+    "disco",
+    "house",
+    "jazz",
+    "pop",
+    "rap",
+    "retro",
+    "rock",
+  ];
+
+  const setDefaultSrc = (event) => {
+    event.target.src = defaultImage;
+  };
+
+  return (
+    <div className="radio">
+      <div className="filters">
+        {filters.map((filter) => (
+          <span
+            className={stationFilter === filter ? "selected" : ""}
+            onClick={() => setStationFilter(filter)}
+          >
+            {filter}
+          </span>
+        ))}
+      </div>
+      <div className="stations">
+        {stations &&
+          stations.map((station, index) => {
+            return (
+              <div className="station" key={index}>
+                <div className="stationName">
+                  <img
+                    className="logo"
+                    src={station.favicon}
+                    alt="station logo"
+                    onError={setDefaultSrc}
+                  />
+                  <div className="name">{station.name}</div>
+                </div>
+
+                <AudioPlayer
+                  className="player"
+                  src={station.urlResolved}
+                  showJumpControls={false}
+                  layout="stacked"
+                  customProgressBarSection={[]}
+                  customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                  autoPlayAfterSrcChange={false}
+                />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
